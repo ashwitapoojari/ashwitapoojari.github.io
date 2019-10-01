07af2c041facd57607d2e5e09c739bc930044ac6
Sukesh Salian <ssalian@cohesity.com>
2019-10-01 20:37:54 +0530

    made few changes

--- js/scripts.js
+++ js/scripts.js
@@ -237,7 +237,8 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var itc_kol = {lat: 22.5932759, lng: 88.27027720000001};
+
+    var itc_kol = {lat: 13.215834, lng: 74.990493};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: itc_kol,
@@ -251,7 +252,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var la_fiesta =  {lat: 12.215834, lng: 74.990493};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
